7154b5169b72e413f0fffaa36509b495b20e7653
Papa Smerf <albert.lis@interia.pl>
2019-03-23 20:31:50 +0100

    Add text

---
+++ Projekt/Unity/Test/Assets/text.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class text : MonoBehaviour
+{
+    public TextMeshPro textToPrint;
+    uint counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ++counter;
+        counter %= 1000;
+        textToPrint.text = counter.ToString();
+    }
+}
