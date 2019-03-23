e7255582cc43ddad851887aa9402b33221ef3789
Papa Smerf <albert.lis@interia.pl>
2019-03-23 19:53:34 +0100

    Add working rotation around object

---
+++ Projekt/Unity/Test/Assets/pivotRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pivotRotate : MonoBehaviour
+{
+    float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, -speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Projekt/Unity/Test/Assets/platformRotate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformRotate : MonoBehaviour
+{
+    float speed = 50f;
+    //public Transform holder;
+    //public Transform cube1;
+    //float previousRotation = 0;
+    public GameObject pivot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var holderRotation = holder.rotation.y;
+        //Rigidbody cube = cube1.GetComponent<Rigidbody>();
+        //transform.rotation = holder.rotation;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.Rotate(Vector3.back, speed * Time.deltaTime);
+            transform.RotateAround(pivot.transform.position, transform.TransformDirection(Vector3.back), speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.RotateAround(pivot.transform.position, transform.TransformDirection(Vector3.back), -speed * Time.deltaTime);
+            //transform.Rotate(Vector3.back, -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.RotateAround(pivot.transform.position, Vector3.up, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.RotateAround(pivot.transform.position, Vector3.up, -speed * Time.deltaTime);
+        }
+
+    }
+}
