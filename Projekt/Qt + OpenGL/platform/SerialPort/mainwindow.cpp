9bbb7724c19315fbca5c55dbff2ea1cf74532d77
Albert Lis <bercik828@ow.pl>
2019-04-26 20:58:44 +0200

    Add platform with some bugs

---
+++ Projekt/Qt + OpenGL/platform/SerialPort/mainwindow.cpp
@@ -0,0 +1,49 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QSerialPortInfo>
+#include <QSerialPort>
+#include <serialoptionsdialog.h>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    SerialOptionsDialog serialDialog;
+    serialDialog.setModal(true);
+    serialDialog.exec();
+
+    //ui->label->setText(PortName+" "+QString::number(BaudRate));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionSerial_options_triggered()
+{
+    SerialOptionsDialog serialDialog;
+    serialDialog.setModal(true);
+    serialDialog.exec();
+    //ui->label->setText(PortName+" "+QString::number(BaudRate));
+}
+
+void MainWindow::StartSerial()
+{
+    ui->actionConnect->setEnabled(false);
+    ui->actionSerial_options->setEnabled(false);
+    serial.startSerial();
+}
+void MainWindow::on_actionConnect_triggered()
+{
+    StartSerial();
+}
+
+void MainWindow::on_actionDisconnect_triggered()
+{
+    ui->actionConnect->setEnabled(true);
+    ui->actionSerial_options->setEnabled(true);
+    serial.stopSerial();
+}
---
+++ Projekt/Qt + OpenGL/platform/src/Manipulator.cpp
@@ -0,0 +1,4 @@
+#include "Manipulator.hh"
+
+
+Manipulator   Manip;
