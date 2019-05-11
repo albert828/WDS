c425e1b078f6d66ec3fe1f6f18ffcd2c9155c690
Papa Smerf <albert.lis@interia.pl>
2019-05-11 14:43:18 +0200

    Sync files

--- Projekt/Arduino/serial/serial.ino
+++ Projekt/Arduino/serial/serial.ino
@@ -1,10 +1,18 @@
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600,SERIAL_8E1);
 }
 
+uint8_t vertical = 0, horizontal = 255;
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Test");
+  ++vertical;
+  --horizontal;
+  Serial.print("V");
+  Serial.print(vertical);
+  Serial.print(" H");
+  Serial.print(horizontal);
+  Serial.println();
   delay(500);
 }
