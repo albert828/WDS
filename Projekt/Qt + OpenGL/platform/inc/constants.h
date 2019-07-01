bc39910e5f32311543d43d4f68782ffb559d4e6c
Papa Smerf <albert.lis@interia.pl>
2019-07-01 17:04:57 +0200

    Remove unnecessary files

--- Projekt/Qt + OpenGL/platform/inc/constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-#include <serialthread.h>
-uint8_t  H = 0, V = 0;
-
-#endif // CONSTANTS_H
--- Projekt/Qt + OpenGL/platform/src/Manipulator.cpp
+++
@@ -1,4 +0,0 @@
-#include "Manipulator.hh"
-
-
-Manipulator   Manip;
